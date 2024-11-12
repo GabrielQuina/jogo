cc5583e3f8765f3303c982700ab0c934d08ebe90
GabrielQuina <104407071+GabrielQuina@users.noreply.github.com>
2024-11-12 20:43:10 -0300

    .

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 document.getElementById("tela-inicial").addEventListener("click", function() {
     document.getElementById("tela-inicial").style.display = "none";
-
     document.getElementById("selecao").innerHTML = "<div class='quadro'><div class='coluna'><div class='bubassauro'><img src='fotos/Bubassauro.png' alt=''></div><div class='charmander'><img src='fotos/charmander.png' alt=''></div></div><div class='coluna'><div class='squirtle'><img src='fotos/squirtle.png' alt=''></div><div class='pikachu'><img src='fotos/pikachu.png' alt=''></div></div></div>";
+    document.getElementById("selecao").style.display = "none";
 });
