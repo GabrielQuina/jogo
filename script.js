543b7c52135b93ca0325716eff9e08b2bccf168f
GabrielQuina <104407071+GabrielQuina@users.noreply.github.com>
2024-11-29 21:02:29 -0300

    .

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 document.getElementById("tela-inicial").addEventListener("click", function() {
-    document.getElementById("tela-inicial").style.display = "none";
-    document.getElementById("selecao").innerHTML = "<div class='quadro'><div class='coluna'><div class='bubassauro'><img src='fotos/Bubassauro.png' alt=''></div><div class='charmander'><img src='fotos/charmander.png' alt=''></div></div><div class='coluna'><div class='squirtle'><img src='fotos/squirtle.png' alt=''></div><div class='pikachu'><img src='fotos/pikachu.png' alt=''></div></div></div>";
-    document.getElementById("selecao").style.display = "none";
+    document.getElementById("tela-inicial").classList.add("hidden");
+    // executar o código para a tela de seleção depois de 1 segundo
+
+    setTimeout(function() {
+        window.location.href = "selecao.html";
+    },800);
 });
