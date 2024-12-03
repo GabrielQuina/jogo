a637db9b7ba78dede70c975174cbc3f8229185fc
GabrielQuina <104407071+GabrielQuina@users.noreply.github.com>
2024-12-02 21:27:19 -0300

    alteração e incremente de alternação das cores de seleção de personagens

--- script.js
+++ script.js
@@ -4,6 +4,13 @@ function selecionarPersonagem(personagem) {
         document.getElementById("playerVez").value = 2;
         document.getElementById("playerVezlabel").innerHTML = "2";
         sessionStorage.setItem("personagem1", personagem);
+        document.getElementById("indicadorPlayer").style.color = "red";
+        // pega todos elementos com a classname pokemon e remove a classname hoverBlue e add a classname hoverRed
+        var elementos = document.getElementsByClassName("pokemon");
+        for (var i = 0; i < elementos.length; i++) {
+            elementos[i].classList.remove("hoverBlue");
+            elementos[i].classList.add("hoverRed");
+        }        
     } else if (x == 2) {
         sessionStorage.setItem("personagem2", personagem);
         window.location.href = "batalha.html";
